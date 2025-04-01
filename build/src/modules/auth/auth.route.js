b0e3bbaf303b149bea1fc74ebc68d3f89b2b8835
Hoang Tien Lang <langhoangtien@gmail.com>
2025-04-01 14:31:23 +0700

    remove build

--- build/src/modules/auth/auth.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import authController from "./auth.controller.js";
-const router = express.Router();
-router.get("/me", authController.me);
-router.post("/login", authController.login);
-export default router;
-//# sourceMappingURL=auth.route.js.map
