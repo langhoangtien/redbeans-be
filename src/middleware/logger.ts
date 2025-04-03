b029a4dc556c6144233178b14dcbeaa60adeb519
Hoang Tien Lang <langhoangtien@gmail.com>
2025-04-03 23:10:58 +0700

    dfds

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -6,7 +6,7 @@ const logger = pino({
   level: isProduction ? "info" : "debug",
   timestamp: pino.stdTimeFunctions.isoTime, // Hiển thị thời gian đẹp ISO 8601
   ...(isProduction && {
-    destination: pino.destination("./logs/app.log"), // Ghi log vào file khi production
+    destination: pino.destination("./app.log"), // Ghi log vào file khi production
   }),
   transport: undefined, // Không sử dụng transport ở cả dev và prod
 });
