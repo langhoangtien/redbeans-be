aadf4df02e86098c5f3f2654c80ddb3cd53e956c
Hoang Tien Lang <langhoangtien@gmail.com>
2025-10-30 16:04:21 +0700

    update review rating score 2

--- src/routes/public.route.ts
+++ src/routes/public.route.ts
@@ -49,7 +49,7 @@ router.post(
 );
 
 router.get("/reviews", reviewController.getAll);
-router.get("/reviews/:productId", reviewController.getProductRating);
+router.get("/rating/:productId", reviewController.getProductRating);
 router.get("/products", productController.getAll);
 router.get("/products/:id", productController.findOne);
 router.post("/auth/login", authController.login);
